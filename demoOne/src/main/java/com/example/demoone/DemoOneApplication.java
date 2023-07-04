3e3ace22d854393f2fa65ec6fa67f68601e60415
sunwenpu <sunwenpu@chaoxing.com>
2023-07-04 17:37:56 +0800

    2023-7-4测试

--- demoOne/src/main/java/com/example/demoone/DemoOneApplication.java
+++ demoOne/src/main/java/com/example/demoone/DemoOneApplication.java
@@ -1,16 +1,27 @@
 package com.example.demoone;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 //import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@EnableMongoRepositories
 public class DemoOneApplication {
+    public static void main(String[] args) throws Exception {
+        try{
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoOneApplication.class, args);
-        System.out.println("启动成功");
+        }catch (Exception e){
+            try{
+
+            }catch (Exception exception){
+                throw new Exception();
+            }
+        }
     }
-
 }
--- demoOne/src/main/java/com/example/demoone/control/test.java
+++ demoOne/src/main/java/com/example/demoone/control/test.java
@@ -6,6 +6,7 @@ import com.rabbitmq.client.BlockedCallback;
 import io.netty.util.internal.StringUtil;
 import org.apache.logging.log4j.CloseableThreadContext;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
@@ -15,14 +16,12 @@ import java.util.stream.Stream;
 
 public class test {
     private static List<String> LIST = Arrays.asList("https://", "www", ".", "javastack", ".", "cn");
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String command = "chmod 777 " + "C:\\Users\\DELL\\Desktop\\bug";
+        int result = 0;
+        result = Runtime.getRuntime().exec(command).waitFor();
+        System.out.println(result);
 
-        if(false || true && true){
-            System.out.println(111);
-        }
-//111111111111111111111111
-//212
-//333333333333333333333333
 
     }
 }
