ee839177f70247712e0325465717f71f9a3023fd
sunwenpu <sunwenpu@chaoxing.com>
2023-07-14 15:17:44 +0800

    Revert "测试 revert"
    This reverts commit 479662fdf332d6c5eb0e834d9a5f481c46fd907a.

--- demoOne/src/main/java/com/example/demoone/DemoOneApplication.java
+++ demoOne/src/main/java/com/example/demoone/DemoOneApplication.java
@@ -2,12 +2,26 @@ package com.example.demoone;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@EnableMongoRepositories
 public class DemoOneApplication {
     public static void main(String[] args) throws Exception {
-//        111111111111111111
+        try{
+
+        }catch (Exception e){
+            try{
+
+            }catch (Exception exception){
+                throw new Exception();
+            }
+        }
     }
 }
